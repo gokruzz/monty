2cf8b88cffefec0cba224fa6855d2e214790acda
gokruzz <johnsonodukoyatemitope@gmail.com>
2024-01-17 18:30:45 -0800

    swap, add & nop

--- execute.c
+++ execute.c
@@ -11,7 +11,8 @@ int execute(char *content, stack_t **stack, unsigned int line_number, FILE *file
 {
 	instruction_t opst[] = {
 				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-				{"pop", f_pop}, {"queue", f_queue},
+				{"pop", f_pop}, {"swap", f_swap}, {"add", f_add},
+				{"nop", f_nop}, {"queue", f_queue},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
